5ff01ab519d8e5ef05e6addf73163253e1432ce6
himanshuguptaAntino <himanshu.gupta@antino.com>
2023-12-08 16:49:31 +0530

    add bit component in home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Button  from './components/Button/Button';
+import { MyFirstComponent } from '@hg77080/getting-button.my-first-component';
 
 function App() {
   return (
     <div className="App">
-     <Button/>
+      <div style={{width:"100px"}}>
+
+      <MyFirstComponent>HEy!</MyFirstComponent>
+      </div>
+     <Button label={'Hello'}/>
     </div>
   );
 }
-
+ 
 export default App;
